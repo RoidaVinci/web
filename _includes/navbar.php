6190f5399d0741a83b4437905f3c68ce1f9c2424
RoidaVinci <99834999+RoidaVinci@users.noreply.github.com>
2024-08-07 09:43:27 +0200

    Update navbar.php

--- _includes/navbar.php
+++ _includes/navbar.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
-        <li><a href="/index.php">Home</a></li>
-        <li><a href="/articles.php">Articles</a></li>
-        <li><a href="/cv.php">CV</a></li>
+        <li><a href="/index.md">Home</a></li>
+        <li><a href="/article.md">Articles</a></li>
+        <li><a href="/cv.md">CV</a></li>
     </ul>
 </nav>
