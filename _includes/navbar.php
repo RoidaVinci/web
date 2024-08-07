acf5adf984d4ebfe272ac27308224c3bfe529b1e
RoidaVinci <99834999+RoidaVinci@users.noreply.github.com>
2024-08-07 09:53:58 +0200

    Update and rename navbar.html to navbar.php

--- _includes/navbar.php
+++ _includes/navbar.php
@@ -1,7 +1,9 @@
-<nav>
+<?php
+echo '<nav>
     <ul>
         <li><a href="/index.md">Home</a></li>
         <li><a href="/article.md">Articles</a></li>
         <li><a href="/cv.md">CV</a></li>
     </ul>
-</nav>
+</nav>';
+?>
